d459d3f83fc05c5efb82fa5a80eedd7478c3abb1
Suleyman Suleymanzade <sul_soft@hotmail.com>
2024-04-14 11:48:40 +0400

    created Message skeleton

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,35 @@ mod message_passing{
     pub mod message;
 }
 
-use message_passing::message::{Message, MEHolder};
+use message_passing::message::{Message};
 
-
+#[allow(dead_code)]
 #[derive(Debug, Clone)]
 struct Person {
     name: String,
     surname: String,
 }
 
-impl MEHolder for Person {}
-
-
 fn main() {
-    let message = Message::new(
-        Box::new(Person {
-            name: "Suleyman".to_string(),
-            surname: "Suleymanzade".to_string(),
-        })
-    );
 
+    let mut p: Person = Person{
+        name:"S".to_string(), 
+        surname:"sn".to_string()
+    };
+
+    let any_value = Message::new(42);
+
+    let mut any_value = Message::new("Hello, Rust!");
+
+    let res = any_value.get_content::<&str>();
+    println!("{}", res.unwrap());
+
+
+    any_value.set_param("first", "hohoho");
+
+    let res = any_value.get_param::<&str>("first")
+        .unwrap();
+
+    println!("{}", res);
 
 }
--- src/message_passing/message.rs
+++ src/message_passing/message.rs
@@ -1,26 +1,42 @@
+use std::any::Any;
 use std::collections::HashMap;
 
-
-pub trait MEHolder: std::fmt::Debug {
-    // your trait definition here
-}
-
 pub struct Message {
-    content: Box<dyn MEHolder>,
+    content: Box<dyn Any>,
+    params: HashMap<String, Box<dyn Any>>,
 }
 
 impl Message {
-    pub fn new(content: Box<dyn MEHolder>) -> Self {
-        Message { content }
+    pub fn new<T:Any>(data: T) -> Self{
+        Message{
+            content: Box::new(data),
+            params: HashMap::new(),
+        }
     }
 
-    pub fn get_message(&self) -> &Box<dyn MEHolder> {
-        &self.content
+    pub fn get_content<T:Any>(&mut self) -> Option<&mut T>{
+        self.content.downcast_mut()
     }
+
+    pub fn set_content<T:Any>(&mut self, value: T){
+        self.content = Box::new(value);
+    }
+
+    pub fn set_param<T:Any>(&mut self, key:&str, val:T){
+        self.params.insert(key.to_string(), Box::new(val));
+    }
+
+    pub fn get_param<T: Any>(&mut self, key: &str) -> Option<&mut T> {
+        if let Some(par) = self.params.get_mut(key) {
+            if let Some(inner) = par.downcast_mut::<T>() {
+                Some(inner)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
 }
-
-
-
-
-
 
